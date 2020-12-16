e882e7adea2e852ba3f737ebf1b600076f90e4e0
kohei.shiga <koheiandres381022@gmail.com>
2020-12-16 04:09:20 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
             redirect_to login_url
         end
     end
+    
+    def count(user)
+        @count_microposts = user.microposts.count
+    end
 end
